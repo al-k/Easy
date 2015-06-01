0572f7ec9d311e051d2158fc6cf6cecceca1818c
Alex <alk@ukr.net>
2015-06-02 02:59:54 +0300

    added 1 class, change 1 class. added 2 enums

---
+++ Easy/src/org/hillel/it/easy/model/entity/CartUnit.java
@@ -0,0 +1,7 @@
+package org.hillel.it.easy.model.entity;
+
+public class CartUnit extends BaseEntity {
+	private Book book;
+	private int quantity;
+
+}
---
+++ Easy/src/org/hillel/it/easy/model/entity/Stock.java
@@ -0,0 +1,11 @@
+package org.hillel.it.easy.model.entity;
+
+import java.util.List;
+
+public class Stock extends BaseEntity {
+	private List<Book> bookList;
+	private String name;
+	private String address;
+
+}
+//
