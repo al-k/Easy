b0f5a5b4828ff2e0716b1b4cf7d4cf469248a349
Alex <alk@ukr.net>
2015-06-05 14:37:16 +0300

    added some getters/setters, enum

--- Easy/src/org/hillel/it/easy/model/entity/ShoppingCart.java
+++ Easy/src/org/hillel/it/easy/model/entity/ShoppingCart.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 
 public class ShoppingCart extends BaseEntity {
 	private ArrayList<CartUnit> cartUnitsArray; // collection maybe ?
-	private int status; // enum? active-confirmed-toGarbage
+	private ShoppingCartStatus status; // enum 
 	private User user;
+	
+	public ArrayList<CartUnit> getCartUnitsArray() {
+		return cartUnitsArray;
+	}
+	public void setCartUnitsArray(ArrayList<CartUnit> cartUnitsArray) {
+		this.cartUnitsArray = cartUnitsArray;
+	}
+	public ShoppingCartStatus getStatus() {
+		return status;
+	}
+	public void setStatus(ShoppingCartStatus status) {
+		this.status = status;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 }
