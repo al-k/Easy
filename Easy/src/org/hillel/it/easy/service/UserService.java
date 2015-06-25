2dae2606eac6e95c76404b6d81409f7c2ba136e4
Alex <alk@ukr.net>
2015-06-26 00:33:14 +0300

    first steps in services

--- Easy/src/org/hillel/it/easy/service/UserService.java
+++ Easy/src/org/hillel/it/easy/service/UserService.java
@@ -3,5 +3,8 @@ package org.hillel.it.easy.service;
 import org.hillel.it.easy.persistance.repository.UserRepository;
 
 public interface UserService extends UserRepository{
+	public boolean login(String email);
+	public boolean registration(String email);
+	
 
 }
---
+++ Easy/src/org/hillel/it/easy/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package org.hillel.it.easy.service.impl;
+
+import org.hillel.it.easy.model.entity.User;
+import org.hillel.it.easy.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean add(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User getByEmail(String userEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean login(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean registration(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
