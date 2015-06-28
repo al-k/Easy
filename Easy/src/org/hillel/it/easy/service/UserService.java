4f98974e5b101eba30766163757fedaf524ee32b
Alex <alk@ukr.net>
2015-06-29 00:13:48 +0300

    immplemented UserService

--- Easy/src/org/hillel/it/easy/service/UserService.java
+++ Easy/src/org/hillel/it/easy/service/UserService.java
@@ -1,10 +1,12 @@
 package org.hillel.it.easy.service;
 
-import org.hillel.it.easy.persistance.repository.UserRepository;
+import org.hillel.it.easy.model.entity.Order;
+import org.hillel.it.easy.model.entity.User;
 
-public interface UserService extends UserRepository{
-	public boolean login(String email);
-	public boolean registration(String email);
-	
+public interface UserService{
+	User login(String UserEmail, String password);
+	boolean registration( User user);
+	boolean addOrder(Order order);
+	boolean updateOrder(Order order);	
 
 }
--- Easy/src/org/hillel/it/easy/service/impl/UserServiceImpl.java
+++ Easy/src/org/hillel/it/easy/service/impl/UserServiceImpl.java
@@ -1,44 +1,41 @@
 package org.hillel.it.easy.service.impl;
 
+import org.hillel.it.easy.model.entity.Order;
 import org.hillel.it.easy.model.entity.User;
+import org.hillel.it.easy.persistance.inmemory.InMemoryOrderRepository;
+import org.hillel.it.easy.persistance.inmemory.InMemoryUserRepository;
+import org.hillel.it.easy.persistance.repository.OrderRepository;
+import org.hillel.it.easy.persistance.repository.UserRepository;
 import org.hillel.it.easy.service.UserService;
 
 public class UserServiceImpl implements UserService {
+	private UserRepository dBaseUser = new InMemoryUserRepository();
+	private OrderRepository dBaseOrder = new InMemoryOrderRepository();
 
 	@Override
-	public boolean add(User user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean update(User user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public User getByEmail(String userEmail) {
-		// TODO Auto-generated method stub
+	public User login(String userEmail, String password) {
+		User user =	dBaseUser.getByEmail(userEmail);
+		if (user!= null) {
+			if (user.getPassword().equals(password)) {
+				return user;
+			}
+		}
 		return null;
 	}
 
 	@Override
-	public boolean delete(User user) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean registration(User user) {
+		return dBaseUser.add(user);
 	}
 
 	@Override
-	public boolean login(String email) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean addOrder(Order order) {
+		return dBaseOrder.add(order);
 	}
 
 	@Override
-	public boolean registration(String email) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean updateOrder(Order order) {
+		return dBaseOrder.update(order);
 	}
 
 }
